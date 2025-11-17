24295bfcbfa2cf8b346c4e664fab73bdadf32aed
SurajVishw <surajvishwakarma10234@gmail.com>
2025-11-17 23:06:58 +0530

    TabButton Updated

--- 01-React Essentials/src/App.jsx
+++ 01-React Essentials/src/App.jsx
@@ -1,8 +1,13 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -15,10 +20,27 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <TabButton onSelect={handleSelect}>Components</TabButton>
+  this code will run when the click event occur 
+
+<TabButton onSelect={()=>handleSelect(component)}>Components</TabButton> 
+  in this code first ananomus function will get render not the function inside it when click or any event occur only then the function get executed
+
+*/
+}
